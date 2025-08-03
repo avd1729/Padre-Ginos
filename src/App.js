a3f9a8790d46508b6bd25f9af8c16e2ff46add22
Aravind <aravind19112003@gmail.com>
2025-08-03 17:39:54 +0530

    npm init & prettier

--- src/App.js
+++ src/App.js
@@ -1,44 +1,41 @@
 const Pizza = (props) => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h2", {}, props.name),
-            React.createElement("p", {}, props.description)
-        ]
-    )
-}
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
 
 // Define a functional component named 'App'
 const App = () => {
-    return React.createElement(
-        "div",                  // The HTML element to create: a <div>
-        {},                     // Props for the <div> (empty here)
-        React.createElement(    // Child element: another React element
-            "h1",               // The HTML element to create: <h1>
-            {},                 // Props for the <h1> (none used)
-            "Padre Gino's"      // Content/text inside <h1>
-        ),
-        React.createElement(Pizza, {
-            name: "The Pepperoni Pizza",
-            description: "Mozzarella Cheese, Pepperoni",
-        }),
-        React.createElement(Pizza, {
-            name: "The Hawaiian Pizza",
-            description: "Sliced Ham, Pineapple, Mozzarella Cheese",
-        }),
-        React.createElement(Pizza, {
-            name: "The Big Meat Pizza",
-            description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
-        }),
-    )
-}
+  return React.createElement(
+    "div", // The HTML element to create: a <div>
+    {}, // Props for the <div> (empty here)
+    React.createElement(
+      // Child element: another React element
+      "h1", // The HTML element to create: <h1>
+      {}, // Props for the <h1> (none used)
+      "Padre Gino's" // Content/text inside <h1>
+    ),
+    React.createElement(Pizza, {
+      name: "The Pepperoni Pizza",
+      description: "Mozzarella Cheese, Pepperoni",
+    }),
+    React.createElement(Pizza, {
+      name: "The Hawaiian Pizza",
+      description: "Sliced Ham, Pineapple, Mozzarella Cheese",
+    }),
+    React.createElement(Pizza, {
+      name: "The Big Meat Pizza",
+      description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
+    })
+  );
+};
 
 // Get the HTML element with id="root" from the DOM
-const container = document.getElementById("root")
+const container = document.getElementById("root");
 
 // Create a root for React to render the app into (React 18+ syntax)
-const root = ReactDOM.createRoot(container)
+const root = ReactDOM.createRoot(container);
 
 // Render the App component into the root container
-root.render(React.createElement(App))
+root.render(React.createElement(App));
