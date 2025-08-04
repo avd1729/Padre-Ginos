a7994fd2e22f770d140749174631ac36012cc908
Aravind <aravind19112003@gmail.com>
2025-08-04 10:24:11 +0530

    modify eslint config for react

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,11 +7,24 @@ import globals from "globals"
 // Import Prettier config to disable ESLint rules that conflict with Prettier formatting
 import prettier from "eslint-config-prettier"
 
+import reactPlugin from "eslint-plugin-react"
+
 // Exporting an array of ESLint config objects (flat config format)
 /** @type {import('eslint').Linter.Config[]} */
 export default [
     // Use ESLint's recommended base configuration for JS
     js.configs.recommended,
+
+    {
+        ...reactPlugin.configs.flat.recommended,
+        settings: {
+            react: {
+            version: "detect",
+            },
+        },
+    },
+
+    reactPlugin.configs.flat["jsx-runtime"],
 
     // Custom configuration applied to JS/TS/JSX/TSX files
     {
@@ -26,7 +39,11 @@ export default [
                     jsx: true, // Enable parsing of JSX syntax (used in React)
                 }
             }
-        }
+        },
+        rules: {
+            "react/no-unescaped-entities": "off",
+            "react/prop-types": "off",
+        },
     },
 
     // Apply Prettier config to disable conflicting formatting rules
